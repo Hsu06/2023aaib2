bb3a8f5d74dbf3200ba9ee815453e7f323dc0cd1
Hsu06 <112918645+Hsu06@users.noreply.github.com>
2024-03-22 14:49:27 +0800

    week03

--- week03/week03-6.cpp
+++ week03/week03-6.cpp
@@ -1,11 +1,20 @@
 #include <stdio.h>
 int main()
 {
-	int N;
-	scanf("%d",&N);
-	int ans=1;
-	for(int i=1;i<=N;i++){
-		ans*=i;
-		}
-		printf("%d",ans);
-		}
+    int N;
+    scanf("%d",&N);
+    int ans=1;
+    for(int i=1; i<=N; i++)
+    {
+        ans*=i;
+    }
+    printf("%d",ans);
+}
+
+
+//python
+a=int(input())
+ans=1
+for i in range(1,a+1,1):
+		ans*=i
+print(ans,end='')
